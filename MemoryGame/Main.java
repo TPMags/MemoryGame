dc7de203337ef84188e225a0bab1961c270caf4c
JM <Not given>
2025-05-09 11:25:04 -0400

    working rows

--- MemoryGame/Main.java
+++ MemoryGame/Main.java
@@ -11,22 +11,24 @@ public class Main {
         MemoryGameModel model = new MemoryGameModel();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose view mode: 1. Text  2. GUI");
+        System.out.println("Choose view mode: 1. Text  2. GUI;");
+        System.out.println("Then choose number of rows.");
 
         try {
             int choice = scanner.nextInt();
+            int rows = scanner.nextInt();
 
             if (choice == 1) {
-                view = new MemoryGameTextView(3);
+                view = new MemoryGameTextView(rows);
                 controller = new MemoryGameController(model, view, 9);
                 controller.play();
             } else if (choice == 2) {
-                view = new MemoryGameGUIView(3);
+                view = new MemoryGameGUIView(rows);
                 controller = new MemoryGameController(model, view, 9);
                 controller.play();
             } else {
                 System.out.println("Invalid choice. Defaulting to Text View.");
-                view = new MemoryGameTextView(3);
+                view = new MemoryGameTextView(rows);
                 controller = new MemoryGameController(model, view, 9);
                 controller.play();
             }
